ddc6c18531eac1dc8d794881ab32509eeb9e6de9
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-07-27 13:58:46 +0200

    add file via upload
    static files for my portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+let themeDots=document.getElementsByClassName('theme-dot')
+
+let theme=localStorage.getItem('theme')
+if(theme==null){
+    setTheme('light')
+}
+else{
+    setTheme(theme)
+}
+
+
+for(var i=0;themeDots.length>i;i++){
+    themeDots[i].addEventListener('click',function(){
+        let mode =this.dataset.mode
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode=='light'){
+        document.getElementById('theme-style').href='default.css'
+    }
+    if(mode=='dark'){
+        document.getElementById('theme-style').href='dark.css'
+    }
+    localStorage.setItem('theme',mode)
+}
+
